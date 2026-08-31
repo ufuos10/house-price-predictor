fb9f64cf0394b8c80416f6d4ed9de87bfbe6b802
Eghalemena Moses <masonnofx@hotmail.com>
2026-09-01 00:41:06 +0100

    expose a new port so that prometheus metrics can listen

--- src/api/main.py
+++ src/api/main.py
@@ -3,6 +3,8 @@ from fastapi.middleware.cors import CORSMiddleware
 from inference import predict_price, batch_predict
 from schemas import HousePredictionRequest, PredictionResponse
 from prometheus_fastapi_instrumentator import Instrumentator #  👈 Add this
+from prometheus_client import start_http_server
+import threading
 
 # Initialize FastAPI app with metadata
 app = FastAPI(
@@ -35,6 +37,12 @@ app.add_middleware(
 
 # Initialize and instrument Prometheus metrics
 Instrumentator().instrument(app).expose(app) # 👈 Add this
+
+# Start Prometheus metrics server in a background thread on port 9100
+def start_metrics_server():
+    start_http_server(9100)
+
+threading.Thread(target=start_metrics_server, daemon=True).start()
 
 # Health check endpoint
 @app.get("/health", response_model=dict)
